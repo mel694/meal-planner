9e3ad4ffe473890e6eb3bd2d2e35b23c8790bc79
Melanie Rollason <melanierollason@MacBook-Air.local>
2026-05-25 22:13:56 +0100

    fix: update middleware for Clerk v7

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,8 @@ const isProtectedRoute = createRouteMatcher([
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
-    await auth.protect();
+    const { protect } = await auth();
+    await protect();
   }
 });
 
